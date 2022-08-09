059060b7eba66976816b428a437fa7c10a439917
andersonmacedo060617 <dasilvamacedo.anderson@gmail.com>
2022-08-09 16:32:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
 
-rand=np.random.normal(1, 2, size=20)
-fig, ax = plt.subplots()
-ax.hist(rand, bins=15)
-st.pyplot(fig)
+st.balloons()
+st.progress(10)
+with st.spinner('Wait for it...'):
+    time.sleep(10)
